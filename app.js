243d6c510e37da08a587db8b14ecf2dfd27473bf
Bowen <huangbowen521@gmail.com>
2013-06-06 19:04:58 +0800

    add index request

--- app.js
+++ app.js
@@ -2,6 +2,10 @@ var express = require('express');
 var app = express();
 var gcmSender = require('./gcm');
 
+app.get('/', function(req, res) {
+   res.send("Hello, it works! visit /ap/open to open ap, visit /ap/close to close ap.");
+});
+
 app.get('/ap/open', function(req, res) {
    gcmSender.send('open', function(result) {
        res.send(result);
